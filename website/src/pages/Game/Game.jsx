c22ebdad39ee5f1e8ab49a01999221c717ef24f6
GregoryToc <115637044+GToccanierIsitech@users.noreply.github.com>
2023-01-09 17:30:22 +0100

    Page Game Commencer

--- website/src/pages/Game/Game.jsx
+++ website/src/pages/Game/Game.jsx
@@ -1,10 +1,36 @@
+import { useState } from 'react'
 import './Game.scss'
 
 function Game() {
+    const [cards, setCards] = useState(
+        [
+            { color: "blue", value: "Skip", hexa: "#178ccd" },
+            { color: "red", value: "8", hexa: "#cb1f45" },
+            { color: "blue", value: "8", hexa: "#178ccd" },
+            { color: "blue", value: "8", hexa: "#178ccd" },
+            { color: "red", value: "8", hexa: "#cb1f45" },
+            { color: "green", value: "2", hexa: "#e8ab28" },
+            { color: "blue", value: "3", hexa: "#178ccd" },
+            { color: "green", value: "4", hexa: "#168d53" }
+        ]
+    )
 
-
+    function AfficherCarte() {
+        return (cards.map((card) => (
+            <div className='card' style={{ backgroundColor: card.hexa }}>
+                <p>{card.value}</p>
+            </div>
+        )
+        ))
+    }
     return (
-        <div className="LobbyPage">
+        <div className="GamePage">
+            <div className='cards-bottom'>
+                {AfficherCarte()}
+            </div>
+            <div className='cards-top'>
+                {AfficherCarte()}
+            </div>
         </div>
     )
 }
